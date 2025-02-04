3262f6029dd911648f5a2a3df64793afeedb1f83
shallomkanyori <shallomkanyori@gmail.com>
2025-02-04 18:49:34 +0300

    Setup Payment model

--- alx_travel_app/listings/models.py
+++ alx_travel_app/listings/models.py
@@ -34,3 +34,12 @@ class Review(models.Model):
     rating = models.IntegerField()
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+class Payment(models.Model):
+    payment_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    booking_id = models.ForeignKey('Booking', on_delete=models.CASCADE)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=50)
+    payment_status = models.CharField(max_length=50)
+    transaction_id = models.CharField(max_length=255)
