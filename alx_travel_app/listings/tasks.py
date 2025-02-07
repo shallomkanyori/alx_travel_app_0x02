76922a0bc2a9c27575246a764638418972290bc3
shallomkanyori <shallomkanyori@gmail.com>
2025-02-07 18:03:02 +0300

    Fix runtime errors in sending email task

--- alx_travel_app/listings/tasks.py
+++ alx_travel_app/listings/tasks.py
@@ -1,12 +1,12 @@
 from celery import shared_task
-from .models import Payment
+from .models import Payment, Booking
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 
 @shared_task
 def send_payment_email(transaction_id):
     payment = Payment.objects.get(transaction_id=transaction_id)
-    user = User.objects.get(id=payment.booking_id.user_id)
+    user = payment.booking_id.user_id
     send_mail(
         'Payment Confirmation',
         f'Your payment of {payment.amount} ETB has been received.',
